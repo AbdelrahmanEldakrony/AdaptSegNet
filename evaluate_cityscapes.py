4ea89e5a1474085fec41ed76e5c9241bdfcc9592
Abdelrahman Eldakrony <abdelrahmaneldakrony@gmail.com>
2018-08-13 16:41:32 +0200

    Video Manipulation

--- evaluate_cityscapes.py
+++ evaluate_cityscapes.py
@@ -15,6 +15,7 @@ from model.deeplab_vgg import DeeplabVGG
 from dataset.cityscapes_dataset import cityscapesDataSet
 from collections import OrderedDict
 import os
+import cv2
 from PIL import Image
 
 import matplotlib.pyplot as plt
@@ -40,6 +41,142 @@ palette = [128, 64, 128, 244, 35, 232, 70, 70, 70, 102, 102, 156, 190, 153, 153,
 zero_pad = 256 * 3 - len(palette)
 for i in range(zero_pad):
     palette.append(0)
+
+
+def VideoCapturee():
+    cap=cv2.VideoCapture(r'/media/avidbeam/Workspace/Abdelrahman-Ws/AdaptSegNet/data/Cityscapes/data/leftImg8bit/val/frankfurt/IMG_3198.MOV')
+
+    cnt=0
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        np.rot90(frame)
+        #frame=cv2.flip(frame,0)
+        #cv2.transpose(frame,frame)
+        #frame=cv2.flip(frame,frame, +1)
+        #cv2.imshow('frame',gray)
+        ss = str(cnt)
+        sz = len(ss)
+        kk = "00"
+        for j in range(4 - sz):
+            kk = kk + '0'
+
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        cv2.imwrite(r'/media/avidbeam/Workspace/Abdelrahman-Ws/AdaptSegNet/data/Cityscapes/data/leftImg8bit/val/frankfurt/' +kk + str(cnt)+".png",frame)
+        cnt=cnt+1
+        if(cnt>600):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+def Final_segmentation():
+
+    Ground_Truth=os.listdir(r'/media/avidbeam/Workspace/Abdelrahman-Ws/AdaptSegNet/data/Cityscapes/data/leftImg8bit/val/frankfurt')
+
+    Ground_Truth.sort()
+
+    Colored=os.listdir(r'/media/avidbeam/Workspace/Abdelrahman-Ws/AdaptSegNet/result/cityscapes')
+
+    Colored.sort()
+
+    cnt=0
+
+    for i in range(600):
+
+
+        image_path = os.path.join(r'/media/avidbeam/Workspace/Abdelrahman-Ws/AdaptSegNet/data/Cityscapes/data/leftImg8bit/val/frankfurt', Ground_Truth[i])
+        gt=cv2.imread(image_path)
+        gt = cv2.resize(gt, (600, 600))
+        gt=np.asanyarray(gt)
+
+
+        image_path = os.path.join(r'/media/avidbeam/Workspace/Abdelrahman-Ws/AdaptSegNet/result/cityscapes',Colored[i])
+        cld = cv2.imread(image_path)
+        cld = cv2.resize(cld, (600, 600))
+        #cv2.imshow("oyr", cld)
+        cld = np.asanyarray(cld)
+        Seg = np.zeros([600, 600, 3], dtype="uint8")
+
+        for i in range(600):
+            for j in range(600):
+                if (cld[i][j][0] == 60 and cld[i][j][1] == 20 and cld[i][j][2] == 220):
+                    Seg[i][j][0] = 30 + (gt[i][j][0] * 0.5)
+                    Seg[i][j][1] = 10 + (gt[i][j][1] * 0.5)
+                    Seg[i][j][2] = 110 + (gt[i][j][2] * 0.5)
+                else:
+                    Seg[i][j][0] = gt[i][j][0]
+                    Seg[i][j][1] = gt[i][j][1]
+                    Seg[i][j][2] = gt[i][j][2]
+                    
+        ss = str(cnt)
+        sz = len(ss)
+        kk = "00"
+        for j in range(4 - sz):
+            kk = kk + '0'
+        cv2.imwrite("/media/avidbeam/Workspace/Abdelrahman-Ws/AdaptSegNet/ToBeWritten/" + kk + str(cnt) + ".png", Seg)
+        cnt = cnt + 1
+
+
+
+    
+def VideoWriter():
+
+    print("Entered")
+    #ap = argparse.ArgumentParser()
+    #ap.add_argument("-ext", "--extension", required=False, default='png', help="extension name. default is 'png'.")
+    #ap.add_argument("-o", "--output", required=False, default='output.mp4', help="output video file")
+    #args1 = vars(ap.parse_args())
+    #args1 = ap.parse_args()
+    #print("Here")
+    ext = "png"
+    output = "output.mp4"
+
+
+    dir_path = r'/media/avidbeam/Workspace/Abdelrahman-Ws/AdaptSegNet/ToBeWritten'
+    #ext = args1['extension']
+    #output = args1['output']
+
+    images = []
+    for f in os.listdir(dir_path):
+        if f.endswith(ext):
+            images.append(f)
+
+
+    images.sort()
+    #print(images)
+
+    image_path = os.path.join(dir_path, images[0])
+    frame = cv2.imread(image_path)
+    #cv2.imshow('video',frame)
+    height, width, channels = frame.shape
+
+# Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Be sure to use lower case
+    out = cv2.VideoWriter(output, fourcc, 15, (width, height))
+
+    cnt=0
+    for image in images:
+
+        image_path = os.path.join(dir_path, image)
+        frame = cv2.imread(image_path)
+
+        out.write(frame)
+        #cv2.imshow('video',frame)
+        if (cv2.waitKey(1) & 0xFF) == ord('q'):
+            break
+
+
+    out.release()
+    #cv2.destroyAllWindows()
+
+    print("The output video is {}".format(output))
+
 
 
 def colorize_mask(mask):
@@ -80,6 +217,8 @@ def get_arguments():
 def main():
     """Create the model and start the evaluation process."""
 
+    
+
     args = get_arguments()
 
     gpu0 = args.gpu
@@ -108,9 +247,9 @@ def main():
 
 
     if version.parse(torch.__version__) >= version.parse('0.4.0'):
-        interp = nn.Upsample(size=(1024, 2048), mode='bilinear', align_corners=True)
+        interp = nn.Upsample(size=(720, 1280), mode='bilinear', align_corners=True)
     else:
-        interp = nn.Upsample(size=(1024, 2048), mode='bilinear')
+        interp = nn.Upsample(size=(720, 1280), mode='bilinear')
 
     for index, batch in enumerate(testloader):
         if index % 100 == 0:
@@ -130,9 +269,14 @@ def main():
         output = Image.fromarray(output)
 
         name = name[0].split('/')[-1]
-        output.save('%s/%s' % (args.save, name))
+        #output.save('%s/%s' % (args.save, name))
         output_col.save('%s/%s_color.png' % (args.save, name.split('.')[0]))
 
 
+
 if __name__ == '__main__':
+    VideoCapturee()
     main()
+    Final_segmentation()
+    VideoWriter()
+
